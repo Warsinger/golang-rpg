1cd10f1bf88cc16aac4d1c0df0927da005d2d854
Matt Lee <eelwehttam@gmail.com>
2024-07-05 05:22:13 -0500

    added in treasure and health items

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ const (
 type Game struct {
 	player   *Player
 	monsters []*Monster
+	items    []Usable
 }
 
 type Object struct {
@@ -33,6 +34,7 @@ type Entity struct {
 	defense       int
 	maxHealth     int
 	currentHealth int
+	gold          int
 }
 type Attacker struct {
 	attack int
@@ -44,6 +46,51 @@ type Player struct {
 type Monster struct {
 	Entity
 	Attacker
+}
+
+type Item struct {
+	Object
+	value int
+	used  bool
+}
+
+type Usable interface {
+	Draw(screen *ebiten.Image)
+	Select(screen *ebiten.Image)
+
+	Use(e *Entity)
+	inRange(e *Entity) bool
+	Refresh()
+}
+
+type Treasure struct {
+	Item
+}
+
+type Health struct {
+	Item
+}
+
+func (h *Health) Use(e *Entity) {
+	if !h.used {
+		e.currentHealth = int(math.Min(float64(e.currentHealth+h.value), float64(e.maxHealth)))
+		h.used = true
+	}
+}
+
+func (t *Treasure) Use(e *Entity) {
+	if !t.used {
+		e.gold += t.value
+		t.used = true
+	}
+}
+
+func (i *Item) Refresh() {
+	i.used = false
+}
+
+func (i *Item) inRange(e *Entity) bool {
+	return inRange(&i.Object, &e.Object)
 }
 
 var (
@@ -79,7 +126,7 @@ func (e *Entity) DrawInfo(screen *ebiten.Image, textOffset func() (float32, floa
 	// Draw health inside the character
 	var infoText string
 	if e.currentHealth > 0 {
-		infoText = fmt.Sprintf("%s\n%d/%d", e.name, e.currentHealth, e.maxHealth)
+		infoText = fmt.Sprintf("%s\n%d/%d\n%dg", e.name, e.currentHealth, e.maxHealth, e.gold)
 	} else {
 		infoText = fmt.Sprintf("Dead\n%s", e.name)
 	}
@@ -99,10 +146,37 @@ func (m *Monster) Draw(screen *ebiten.Image) {
 	m.DrawInfo(screen, m.TextOffset)
 
 }
+func (t *Treasure) Draw(screen *ebiten.Image) {
+	drawItem(screen, &t.Item, color.RGBA{255, 215, 0, 255})
+
+	// t.DrawInfo(screen, t.TextOffset)
+
+}
+func (h *Health) Draw(screen *ebiten.Image) {
+	drawItem(screen, &h.Item, color.RGBA{100, 255, 100, 255})
+
+	// t.DrawInfo(screen, t.TextOffset)
+}
+
+func drawItem(screen *ebiten.Image, i *Item, c color.Color) {
+	if !i.used {
+		offset := i.size / 2
+		vector.DrawFilledRect(screen, i.x-offset, i.y-offset, i.size, i.size, c, true)
+	}
+}
 
 func (m *Monster) Select(screen *ebiten.Image) {
-	offset := m.size / 2
-	vector.StrokeRect(screen, m.x-offset, m.y-offset, m.size, m.size, 2, color.RGBA{0, 255, 255, 255}, true)
+	if m.Alive() {
+		offset := m.size / 2
+		vector.StrokeRect(screen, m.x-offset, m.y-offset, m.size, m.size, 2, color.RGBA{0, 255, 255, 255}, true)
+	}
+}
+
+func (i *Item) Select(screen *ebiten.Image) {
+	if !i.used {
+		offset := i.size / 2
+		vector.StrokeRect(screen, i.x-offset, i.y-offset, i.size, i.size, 2, color.RGBA{0, 255, 255, 255}, true)
+	}
 }
 
 func (e *Monster) TextOffset() (float32, float32) {
@@ -110,11 +184,14 @@ func (e *Monster) TextOffset() (float32, float32) {
 	y := e.y - e.size/2
 	return x, y
 }
+
 func (p *Player) TextOffset() (float32, float32) {
-	x := p.x - p.size*2
-	y := p.y - p.size/2
-	return x, y
+	// x := p.x - p.size*2
+	// y := p.y - p.size/2
+	// return x, y
+	return 4, 4
 }
+
 func (p *Player) Draw(screen *ebiten.Image) {
 	var c color.Color
 	if p.Alive() {
@@ -147,8 +224,19 @@ func (g *Game) Update() error {
 
 		if ebiten.IsKeyPressed(ebiten.KeyA) {
 			for _, m := range g.monsters {
-				if inRange(g.player, m) {
+				if m.Alive() && inRange(&g.player.Object, &m.Object) {
 					attack(g.player, m)
+					if !m.Alive() {
+						// drop some treasure
+						g.items = append(g.items, &Treasure{Item: Item{value: m.gold, Object: Object{m.x + m.size/2, m.y + m.size/2, 15}}})
+					}
+				}
+			}
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyU) {
+			for _, i := range g.items {
+				if i.inRange(&g.player.Entity) {
+					i.Use(&g.player.Entity)
 				}
 			}
 		}
@@ -161,10 +249,15 @@ func (g *Game) Init() {
 	g.player.Heal()
 	g.player.x = 50
 	g.player.y = 50
+	g.player.gold = 10
 	for i, m := range g.monsters {
 		m.Heal()
 		m.x = float32(50 * (i + 2))
 		m.y = float32(50 * (i + 2))
+		m.gold = int(m.size)
+	}
+	for _, item := range g.items {
+		item.Refresh()
 	}
 }
 
@@ -186,9 +279,9 @@ func distance(o1, o2 *Object) float64 {
 	return math.Sqrt(x*x + y*y)
 }
 
-func inRange(p *Player, m *Monster) bool {
-	// if the distance between player and monster is < the sum of their sizes they can interact
-	return m.Alive() && p.Alive() && distance(&p.Object, &m.Object) < float64(p.size+m.size)
+func inRange(o1, o2 *Object) bool {
+	// if the distance 2 objects is < the sum of their sizes they can interact
+	return distance(o1, o2) < float64(o1.size+o2.size)
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -196,8 +289,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
 	for _, m := range g.monsters {
 		m.Draw(screen)
-		if inRange(g.player, m) {
+		if inRange(&g.player.Object, &m.Object) {
 			m.Select(screen)
+		}
+	}
+	for _, i := range g.items {
+		i.Draw(screen)
+		if i.inRange(&g.player.Entity) {
+			i.Select(screen)
 		}
 	}
 }
@@ -215,6 +314,11 @@ func main() {
 			{Entity: Entity{name: "Barbol1", Object: Object{size: 16}, defense: 3, maxHealth: 40}, Attacker: Attacker{attack: 2}},
 			{Entity: Entity{name: "Barbol2", Object: Object{size: 16}, defense: 3, maxHealth: 40}, Attacker: Attacker{attack: 2}},
 			{Entity: Entity{name: "Barbol3", Object: Object{size: 16}, defense: 3, maxHealth: 40}, Attacker: Attacker{attack: 2}},
+		},
+		items: []Usable{
+			&Treasure{Item: Item{value: 100, Object: Object{15, 344, 15}}},
+			&Health{Item: Item{value: 50, Object: Object{127, 65, 15}}},
+			&Health{Item: Item{value: 50, Object: Object{324, 44, 15}}},
 		},
 	}
 	game.Init()
